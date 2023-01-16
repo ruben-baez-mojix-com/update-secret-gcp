6ea7f99f6ec40b9b2284246e3e83fff5051798dd
Ruben <ruben.baez@mojix.com>
2023-01-16 09:20:05 -0500

    change read config

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 import sys
 import json
@@ -7,13 +6,13 @@ from google.cloud import secretmanager
 from google.oauth2 import service_account
 
 
-def main(filename, project_id, secret_id, keyfile):
+def main(path, project_id, secret_id, keyfile):
     decoded_key = base64.b64decode(keyfile)
     decoded_str = decoded_key.decode("ascii")
     final_key = json.loads(decoded_str)
     credentials = service_account.Credentials.from_service_account_info(final_key)
-    with open(filename, 'rb') as file:
-        print(f"Updating {filename}..")
+    with open(path, 'rb') as file:
+        print(f"Updating {path}..")
         data = file.read()
         client = secretmanager.SecretManagerServiceClient(credentials=credentials)
         parent = client.secret_path(project_id, secret_id)
@@ -26,22 +25,11 @@ def main(filename, project_id, secret_id, keyfile):
 
 if __name__ == '__main__':
     #Get args
-    parser = argparse.ArgumentParser(description="Update secret")
-    parser.add_argument('file')
-    filename = parser.parse_args().file
-    # key = argparse.ArgumentParser(description="Application credentials")
-    # key.add_argument('key')
-    # keyfile = key.parse_args().file
+    path = os.environ["INPUT_PATH"]
+
     project_id = os.getenv("PROJECT_ID")
     secret_id = os.getenv("SECRET_ID")
     keyfile = os.getenv("KEYFILE")
-    #Validating file
-    # if not os.path.exists(filename):
-    #     print(f"ERROR: file {filename} doesn't exist")
-    #     sys.exit(1)
-    # if not os.path.exists(keyfile):
-    #     print(f"ERROR: key {keyfile} doesn't exist")
-    #     sys.exit(1)
 
-    main(filename, project_id, secret_id, keyfile)
+    main(path, project_id, secret_id, keyfile)
 
